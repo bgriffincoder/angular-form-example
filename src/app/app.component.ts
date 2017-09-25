bf22e5d6a37beca4982265ca6712a88d075127e6
bgriffincoder <bryan.r.griffin@gmail.com>
2017-09-25 09:20:35 -0700

    form submitting data with template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+    //app.component.ts
+    onSubmit(data: NgForm){
+    console.log(data.value)
+  }
 }
+
+
